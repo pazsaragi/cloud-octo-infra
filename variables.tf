43a9342d8ffbb2dd169bbf8c797395717aeaea48
pazsaragi <pazsaragi40@gmail.com>
2021-12-13 19:31:48 +0000

    fmt

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "rds_username" {
-    description = "The username for the RDS instance"
+  description = "The username for the RDS instance"
 }
 
 variable "rds_password" {
-    description = "RDS Password"
+  description = "RDS Password"
 }
 
 variable "apigateway_secret" {
-    description = "The API Gateway Secret"
+  description = "The API Gateway Secret"
 }
